19bd7c05f580c56169a1c22585ed63d3b3ea725b
McMelloy <demetriuszbaranowski@gmail.com>
2020-03-09 10:37:38 +0100

    completed in C#

--- PSMHomework/Task1/Program.cs
+++ PSMHomework/Task1/Program.cs
@@ -4,15 +4,6 @@ namespace Task1
 {
     class Program
     {
-        static int factorial(int number)
-        {
-            int fact = 1;
-            for (int i = 1; i <= number; i++)
-            {
-                fact = fact * i;
-            }
-            return fact;
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Input x:");
@@ -27,20 +18,22 @@ namespace Task1
             x = x > Math.PI/2 ? x - Math.PI : x;
             x = x < -Math.PI / 2 ? x + Math.PI : x; //converting to interval between -pi/2 and pi/2
 
-
+            int numOfFact = 1;
+            int fact = 1;
+            double xInPower = x;
             double result = 0;
             for(int n=0;n<11;n++)
             {
-                double iteration = Math.Pow(-1, n) / factorial(2 * n + 1);
-                iteration *= Math.Pow(x, 2 * n + 1);
+                double iteration = Math.Pow(-1, n) / fact;
+                iteration *= xInPower;
                 result += iteration;
-                Console.WriteLine("Iteration: {0}", n);
-                Console.WriteLine("Result: {0}", result);
+                Console.WriteLine("Iteration: {0}\nResult: {1}", n, result);
+                fact *= (numOfFact + 1) * (numOfFact + 2);
+                numOfFact += 2;
+                xInPower *= x * x;
             }
-            Console.WriteLine();
-            Console.WriteLine("Computer's Result: {0}", Math.Sin(x));
-            Console.WriteLine();
-            Console.WriteLine("Compare: {0}", Math.Abs(result - Math.Sin(x)));
+            Console.WriteLine("\nComputer's Result: {0}", Math.Sin(x));
+            Console.WriteLine("\nComparison with Program's result: {0}", Math.Abs(result - Math.Sin(x)));
 
             Console.ReadKey();
         }
